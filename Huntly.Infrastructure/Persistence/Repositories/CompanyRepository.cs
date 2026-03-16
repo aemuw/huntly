febdc3b892adaaf1989792616b495e7e4b92be55
Artem <artembohun@gmail.com>
2026-03-16 22:09:48 +0200

    feat: add CompanyRepository

---
+++ Huntly.Infrastructure/Persistence/Repositories/CompanyRepository.cs
@@ -0,0 +1,16 @@
+﻿using Huntly.Domain.Entities;
+using Huntly.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Huntly.Infrastructure.Persistence.Repositories
+{
+    public class CompanyRepository : BaseRepository<Company>, ICompanyRepository
+    {
+        public CompanyRepository(AppDbContext context) : base(context) { }
+        public async Task<IReadOnlyList<Company>> GetAllAsync()
+            => await _dbSet
+            .Include(c => c.Technologies)
+            .OrderBy(c => c.Name)
+            .ToListAsync();
+    }
+}
